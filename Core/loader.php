7c23f13032573d82609cff9293fb8012325d2b4d
molanp <molanp>
2024-03-21 22:58:22 +0800

    update font

--- Core/loader.php
+++ Core/loader.php
@@ -4,6 +4,7 @@ $sql = "SELECT name, namespace, method, url, path FROM miku_api WHERE url = :url
 $statement = $DATABASE->prepare($sql);
 $statement->execute([":url" => __API_PATH__]);
 $data = $statement->fetch(PDO::FETCH_ASSOC);
+if (empty($data)) {code(404);}
 if (handle_check($data["name"]) && $_SERVER["REQUEST_METHOD"] == $data["method"] || $data["method"] == "ANY" || in_array($_SERVER["REQUEST_METHOD"], explode('/', $data["method"]))) {
     include_once __MODULE_DIR__ . "/logger.php";
     include_once $data["path"];
